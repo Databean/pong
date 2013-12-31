ced57e94918f0617532ebaddfa83ba8b71fe94e5
A. Cody Schuffelen <astronomix@gmail.com>
2013-12-31 05:09:31 -0800

    Made Menu and MenuItem classes

--- include/menu.h
+++ include/menu.h
@@ -1,8 +1,49 @@
 #ifndef PONG_MENU_H
 #define PONG_MENU_H
 
+#include "GL/gl.h"
+
+#include <vector>
+
+#include "pong.h"
+
 void initMenu();
 
 void drawMenu(bool keyboardState[256]);
+
+class MenuItem {
+private:
+	object obj;
+	GLuint unselectedTexture;
+	GLuint selectedTexture;
+public:
+	MenuItem(const object& obj, GLuint unselectedTexture, GLuint selectedTexture);
+	MenuItem(const MenuItem&) = default;
+	~MenuItem() = default;
+	MenuItem& operator=(const MenuItem&) = default;
+	
+	void select();
+	void unselect();
+	object& getObject();
+};
+
+class Menu {
+private:
+	int selectedRow;
+	int selectedColumn;
+	std::vector<std::vector<MenuItem>> menuItems;
+	object background;
+public:
+	Menu(const object& background);
+	Menu(const Menu&) = default;
+	~Menu() = default;
+	Menu& operator=(const Menu&) = default;
+	
+	void setRows(int rows);
+	void setColumns(int row, int column);
+	MenuItem& operator()(int row, int column);
+	void select(int row, int column);
+	void draw();
+};
 
 #endif
--- src/menu.cpp
+++ src/menu.cpp
@@ -10,14 +10,6 @@ using std::string;
 
 enum { MAIN_MENU, MODE_MENU } menuScreen;
 
-GLuint menuBackground;
-GLuint playTexture;
-GLuint playSelectedTexture;
-GLuint modeTexture;
-GLuint modeSelectedTexture;
-GLuint optionTexture;
-GLuint optionSelectedTexture;
-
 GLuint modeBackground;
 GLuint difficultyTexture;
 GLuint easyTexture;
@@ -40,46 +32,90 @@ object mediumButton;
 object hardButton;
 object extremeButton;
 
+MenuItem::MenuItem(const object& obj, GLuint unselectedTexture, GLuint selectedTexture) : obj(obj), unselectedTexture(unselectedTexture), selectedTexture(selectedTexture) {
+	this->obj.texture = unselectedTexture;
+}
+
+object& MenuItem::getObject() {
+	return obj;
+}
+
+void MenuItem::select() {
+	obj.texture = selectedTexture;
+}
+
+void MenuItem::unselect() {
+	obj.texture = unselectedTexture;
+}
+
+Menu::Menu(const object& background) : background(background) {
+	
+}
+
+void Menu::setRows(int rows) {
+	menuItems.resize(rows);
+}
+
+void Menu::setColumns(int row, int columns) {
+	static object dummyObject = {{0, 0}, {0, 0}, {0, 0}, {0, 0, 0}};
+	static MenuItem dummyMenu(dummyObject, 0, 0);
+	menuItems[row].resize(columns, dummyMenu);
+}
+
+MenuItem& Menu::operator()(int row, int column) {
+	return menuItems[row][column];
+}
+
+void Menu::select(int row, int column) {
+	for(std::vector<MenuItem>& menuRow : menuItems) {
+		for(MenuItem& menuItem : menuRow) {
+			menuItem.unselect();
+		}
+	}
+	selectedRow = row;
+	selectedColumn = column;
+	menuItems[row][column].select();
+}
+
+void Menu::draw() {
+	displayObject(background);
+	for(std::vector<MenuItem>& menuRow : menuItems) {
+		for(MenuItem& menuItem : menuRow) {
+			displayObject(menuItem.getObject());
+		}
+	}
+}
+
+Menu* mainMenu;
+Menu* modeMenu;
+
 bool previousEscape = false;
 
 void initMenu() {
-	menuBackground = loadImage("resources/Menu.png");
-	playTexture = loadImage("resources/Play.png");
-	playSelectedTexture = loadImage("resources/Play selected.png");
-	modeTexture = loadImage("resources/Mode.png");
-	modeSelectedTexture = loadImage("resources/Mode selected.png");
-	optionTexture = loadImage("resources/Option.png");
-	optionSelectedTexture = loadImage("resources/Option selected.png");
+	mainMenu = new Menu({{0.5, 0.5}, {1, 1}, {0, 0}, {1, 1, 1}, loadImage("resources/Menu.png")});
+	mainMenu->setRows(3);
+	for(int i = 0; i < 3; i++) { mainMenu->setColumns(i, 1); }
+	(*mainMenu)(0, 0) = MenuItem({{175/1280., 0.6}, {350./1280.,150./720.}, {0, 0}, {1, 1, 1}, 0}, loadImage("resources/Play.png"), loadImage("resources/Play selected.png"));
+	(*mainMenu)(1, 0) = MenuItem({{175/1280., 0.4}, {350./1280.,150./720.}, {0, 0}, {1, 1, 1}, 0}, loadImage("resources/Mode.png"), loadImage("resources/Mode selected.png"));
+	(*mainMenu)(2, 0) = MenuItem({{175/1280., 0.2}, {350./1280.,150./720.}, {0, 0}, {1, 1, 1}, 0}, loadImage("resources/Option.png"), loadImage("resources/Option selected.png"));
 	
-	modeBackground = loadImage("resources/Mode Menu.png");
+	modeMenu = new Menu({{0.5, 0.5}, {1, 1}, {0, 0}, {1, 1, 1}, loadImage("resources/Mode Menu.png")});
+	modeMenu->setRows(1);
+	modeMenu->setColumns(0, 4);
+	(*modeMenu)(0, 0) = MenuItem({{175/1280., 0.6}, {350/1280., 150/720.}, {0, 0}, {1, 1, 1}, 0}, loadImage("resources/Easy.png"), loadImage("resources/Easy Selected.png"));
+	(*modeMenu)(0, 1) = MenuItem({{425/1280., 0.6}, {350/1280., 150/720.}, {0, 0}, {1, 1, 1}, 0}, loadImage("resources/Medium.png"), loadImage("resources/Medium Selected.png"));
+	(*modeMenu)(0, 2) = MenuItem({{750/1280., 0.6}, {350/1280., 150/720.}, {0, 0}, {1, 1, 1}, 0}, loadImage("resources/Hard.png"), loadImage("resources/Hard Selected.png"));
+	(*modeMenu)(0, 3) = MenuItem({{1000/1280., 0.6}, {350/1280., 150/720.}, {0, 0}, {1, 1, 1}, 0}, loadImage("resources/Extreme.png"), loadImage("resources/Extreme Selected.png"));
+	
 	difficultyTexture = loadImage("resources/Difficulty.png");
-	easyTexture = loadImage("resources/Easy.png");
-	easySelectedTexture = loadImage("resources/Easy Selected.png");
-	mediumTexture = loadImage("resources/Medium.png");
-	mediumSelectedTexture = loadImage("resources/Medium Selected.png");
-	hardTexture = loadImage("resources/Hard.png");
-	hardSelectedTexture = loadImage("resources/Hard Selected.png");
-	extremeTexture = loadImage("resources/Extreme.png");
-	extremeSelectedTexture = loadImage("resources/Extreme Selected.png");
-	
-	bgObject = {{0.5, 0.5}, {1, 1}, {0, 0}, {1, 1, 1}, menuBackground};
-	playButton = {{175/1280., 0.6}, {350./1280.,150./720.}, {0, 0}, {1, 1, 1}, playTexture};
-	modeButton = {{175/1280., 0.4}, {350./1280.,150./720.}, {0, 0}, {1, 1, 1}, modeTexture};
-	optionButton= {{175/1280., 0.2}, {350./1280.,150./720.}, {0, 0}, {1, 1, 1}, optionTexture};
 	
 	difficultyText = {{275/1280., 0.75}, {550/1280., 150/720.}, {0, 0}, {1, 1, 1}, difficultyTexture};
-	easyButton = {{175/1280., 0.6}, {350/1280., 150/720.}, {0, 0}, {1, 1, 1}, easyTexture};
-	mediumButton = {{425/1280., 0.6}, {350/1280., 150/720.}, {0, 0}, {1, 1, 1}, mediumTexture};
-	hardButton = {{750/1280., 0.6}, {350/1280., 150/720.}, {0, 0}, {1, 1, 1}, hardTexture};
-	extremeButton = {{1000/1280., 0.6}, {350/1280., 150/720.}, {0, 0}, {1, 1, 1}, extremeTexture};
 }
 
 void drawMainMenu(bool keyboardState[256]) {
 	static bool previousUp, previousDown;
 	
 	static int selected;
-	
-	bgObject.texture = menuBackground;
 	
 	if(keyboardState['w'] && !previousUp) {
 		selected = (selected + 3 - 1) % 3;
@@ -91,14 +127,8 @@ void drawMainMenu(bool keyboardState[256]) {
 	previousUp = keyboardState['w'];
 	previousDown = keyboardState['s'];
 	
-	playButton.texture = selected == 0 ? playSelectedTexture : playTexture;
-	modeButton.texture = selected == 1 ? modeSelectedTexture : modeTexture;
-	optionButton.texture = selected == 2 ? optionSelectedTexture : optionTexture;
-	
-	displayObject(bgObject);
-	displayObject(playButton);
-	displayObject(modeButton);
-	displayObject(optionButton);
+	mainMenu->select(selected, 0);
+	mainMenu->draw();
 	
 	if(keyboardState[' ']) {
 		if(selected == 0) {
@@ -113,8 +143,6 @@ void drawMainMenu(bool keyboardState[256]) {
 
 void drawModeMenu(bool keyboardState[256]) {
 	static bool previousLeft, previousRight;
-	
-	bgObject.texture = modeBackground;
 	
 	static int column = 0;
 	
@@ -151,17 +179,8 @@ void drawModeMenu(bool keyboardState[256]) {
 	previousLeft = keyboardState['a'];
 	previousRight = keyboardState['d'];
 	
-	easyButton.texture = column == 0 ? easySelectedTexture : easyTexture;
-	mediumButton.texture = column == 1 ? mediumSelectedTexture : mediumTexture;
-	hardButton.texture = column == 2 ? hardSelectedTexture : hardTexture;
-	extremeButton.texture = column == 3 ? extremeSelectedTexture : extremeTexture;
-	
-	displayObject(bgObject);
-	displayObject(difficultyText);
-	displayObject(easyButton);
-	displayObject(mediumButton);
-	displayObject(hardButton);
-	displayObject(extremeButton);
+	modeMenu->select(0, column);
+	modeMenu->draw();
 	
 	if(keyboardState[27] && !previousEscape) { //Escape
 		menuScreen = MAIN_MENU;
